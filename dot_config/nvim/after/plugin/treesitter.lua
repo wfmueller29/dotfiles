a95479038d1c52eeb9552cabf8fa7588ff78aebf
William Mueller <williammueller@williams-mbp.mynetworksettings.com>
2023-11-26 15:00:08 -0500

    Added yaml and bash to treesitter

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "r",
+    ensure_installed = {
+        "r",
         "python",
         "c",
         "lua",
@@ -9,7 +10,10 @@ require 'nvim-treesitter.configs'.setup {
         "query",
         "gitcommit",
         "diff",
-        "git_rebase" },
+        "git_rebase",
+        "yaml",
+        "bash"
+    },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
