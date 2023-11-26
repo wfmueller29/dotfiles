594f34ddc5fa3ac3419abf124da3dfcb90326570
William Mueller <williammueller@williams-mbp.mynetworksettings.com>
2023-11-26 17:49:57 -0500

    Added external plugin file instead of oh-my-zsh

---
+++ dot_config/zsh/plugins.sh
@@ -0,0 +1,5 @@
+# zsh-autosuggestions
+source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-syntax-highlighting
+source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
