bc89f349d8b91ebb6783fd509605d142fe8495ad
William Mueller <williammueller@williams-mbp.mynetworksettings.com>
2023-11-26 21:06:32 -0500

    Updated sketchybar color and removed zsh/plugins

--- dot_config/zsh/plugins.sh
+++
@@ -1,5 +0,0 @@
-# zsh-autosuggestions
-source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# zsh-syntax-highlighting
-source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
