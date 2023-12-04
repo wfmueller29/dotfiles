4a7193839dd6d0786d616366364df069026b7776
William Mueller <williammueller@williams-mbp.mynetworksettings.com>
2023-12-03 22:28:39 -0500

    Added gruvbox and organized plugins

--- dot_config/nvim/lua/theprimeagen/lazy.lua
+++ dot_config/nvim/lua/theprimeagen/lazy.lua
@@ -5,53 +5,53 @@
 
 
 require('lazy').setup({
+    -- File navigation
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.4',
         -- or                            , branch = '0.1.x',
         dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
+    'theprimeagen/harpoon',
+    'mbbill/undotree',
 
+    -- LSP
     --- Uncomment these if you want to manage LSP servers from neovim
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
-
     { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
     { 'neovim/nvim-lspconfig' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
     { 'L3MON4D3/LuaSnip' },
-
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
+    "jalvesaq/Nvim-R",
+    "christoomey/vim-tmux-navigator",
+
+    -- Git
+    'lewis6991/gitsigns.nvim',
+    'tpope/vim-fugitive',
+
+    -- Colorschemes
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { 'rose-pine/neovim', name = 'rose-pine' },
+    'NvChad/nvim-colorizer.lua',
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
     },
 
-    'theprimeagen/harpoon',
-    'mbbill/undotree',
-    'tpope/vim-surround',
-    "jalvesaq/Nvim-R",
-    "christoomey/vim-tmux-navigator",
-    'tpope/vim-fugitive',
-    'NvChad/nvim-colorizer.lua',
-    'lewis6991/gitsigns.nvim',
 
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { 'rose-pine/neovim', name = 'rose-pine' },
+    -- Keybinds
+    'tpope/vim-surround',
     {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end
     },
-
-    {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    },
-
     {
         'tummetott/unimpaired.nvim',
         config = function()
@@ -59,6 +59,10 @@ require('lazy').setup({
                 -- add any options here or leave empty
             }
         end
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
     },
 
 })
