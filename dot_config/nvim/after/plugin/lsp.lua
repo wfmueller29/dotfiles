e3cc3a7535556208c69eba25076014efd542a526
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-01-17 18:24:14 -0500

    Added keymaps for gitsigns, todo, lsp, and harpoon

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,11 +1,5 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {},
@@ -14,10 +8,8 @@ require('mason-lspconfig').setup({
   },
 })
 
-
 local lua_opts = lsp_zero.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
-
 
 -- This will keymap gq to a reformatter
 lsp_zero.on_attach(function(client, bufnr)
@@ -27,4 +19,17 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set({'n', 'x'}, 'gq', function()
     vim.lsp.buf.format({async = false, timeout_ms = 10000})
   end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 end)
