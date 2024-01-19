62f4ee9a11e1df8aae85c6b2b9fe66ae84860623
William Mueller <williammueller@williams-mcp.dhcp.hofstra.edu>
2024-01-19 16:36:05 -0500

    Updated rose-pine colorsheme

--- dot_config/nvim/after/plugin/rose-pine.lua
+++ dot_config/nvim/after/plugin/rose-pine.lua
@@ -1,59 +1,65 @@
 require('rose-pine').setup({
-    --- @usage 'auto'|'main'|'moon'|'dawn'
-    variant = 'auto',
-    --- @usage 'main'|'moon'|'dawn'
-    dark_variant = 'main',
-    bold_vert_split = false,
-    dim_nc_background = false,
-    disable_background = true,
-    disable_float_background = false,
-    disable_italics = false,
+    variant = "auto", -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
 
-    --- @usage string hex value or named color from rosepinetheme.com/palette
-    groups = {
-        background = 'base',
-        background_nc = '_experimental_nc',
-        panel = 'surface',
-        panel_nc = 'base',
-        border = 'highlight_med',
-        comment = 'muted',
-        link = 'iris',
-        punctuation = 'subtle',
-
-        error = 'love',
-        hint = 'iris',
-        info = 'foam',
-        warn = 'gold',
-
-        headings = {
-            h1 = 'iris',
-            h2 = 'foam',
-            h3 = 'rose',
-            h4 = 'gold',
-            h5 = 'pine',
-            h6 = 'foam',
-        }
-        -- or set all headings at once
-        -- headings = 'subtle'
+    styles = {
+        bold = true,
+        italic = true,
+        transparency = true,
     },
 
-    -- Change specific vim highlight groups
-    -- https://github.com/rose-pine/neovim/wiki/Recipes
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        headings = {
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+        },
+        -- Alternatively, set all headings at once.
+        -- headings = "subtle",
+    },
+
     highlight_groups = {
-        ColorColumn = { bg = 'rose' },
+        -- Comment = { fg = "foam" },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+    },
 
-        -- Blend colours against the "base" background
-        CursorLine = { bg = 'foam', blend = 10 },
-        StatusLine = { fg = 'love', bg = 'love', blend = 10 },
-
-        -- By default each group adds to the existing config.
-        -- If you only want to set what is written in this config exactly,
-        -- you can set the inherit option:
-        Search = { bg = 'gold', inherit = false },
-        ["@text.diff.add"] = { fg = "foam", bg = "foam", blend = 20 },
-        ["@text.diff.delete"] = { fg = "love", bg = "love", blend = 20 }
-    }
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
 })
 
 -- Set colorscheme after options
--- vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme rose-pine')
