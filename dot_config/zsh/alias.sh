c04f440b09f9c40b3ad69b611020d1bba137c022
William Mueller <williammueller@williams-mcp.lan>
2025-05-24 19:15:46 -0400

    Created python and pdb alias

--- dot_config/zsh/alias.sh
+++ dot_config/zsh/alias.sh
@@ -1,6 +1,8 @@
 # list of aliases that will be sourced by .zshrc
 
 alias ch="chezmoi"
+alias py="python"
+alias pdb="python -m pdb"
 
 alias bdump="brew bundle dump --force --file=~/.config/brew/dump"
 
