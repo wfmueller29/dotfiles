f6c479168fb5040e6e00a3ef250261f56b751313
William Mueller <williammueller@williams-mcp.local>
2025-05-26 20:55:31 -0400

    Added conform plugin

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,12 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    format_on_save = {
-      timeout_ms = 1000,
-      lsp_fallback = true,
+    "stevearc/conform.nvim",
+    opts = {
+        format_on_save = {
+            timeout_ms = 1000,
+            lsp_fallback = true,
+        },
+        formatters_by_ft = {
+            python = { "ruff_format", "black" }, -- Use Ruff first, then Black
+        },
     },
-    formatters_by_ft = {
-      python = { "ruff_format", "black" },  -- Use Ruff first, then Black
-    },
-  },
 }
-
